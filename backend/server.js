ef6f86343ddf831b8d91ced984f7ab578ce7baf7
Ozioma45 <oziomaegole@gmail.com>
2024-05-02 13:20:26 +0100

    Express router and api Routes

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,20 @@
 const express = require("express");
 require("dotenv").config();
+const workoutRoutes = require("./routes/workouts");
 
 //express app
 const app = express();
 
 //middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 //route handler_react to request
-app.get("/", (req, res) => {
-  res.json({ mssg: "Welcome to the app" });
-});
+app.use("/api/workouts", workoutRoutes);
 
 //listen for requests
 app.listen(process.env.PORT, () => {
