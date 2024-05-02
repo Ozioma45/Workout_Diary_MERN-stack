60230b7026fcedf38c242ad943217c8edb93aa63
Ozioma45 <oziomaegole@gmail.com>
2024-05-02 12:58:56 +0100

    express app setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
-const express = require('express')
+const express = require("express");
+require("dotenv").config();
 
 //express app
-const app = express()
+const app = express();
+
+//middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//route handler_react to request
+app.get("/", (req, res) => {
+  res.json({ mssg: "Welcome to the app" });
+});
 
 //listen for requests
-app.listen(4000, ()=>{
-    console.log("Listening on port 4000")
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
