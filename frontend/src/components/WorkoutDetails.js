f5f29d2ad6eb3e31a9d33576bcf589d269a9c402
Ozioma45 <oziomaegole@gmail.com>
2024-05-04 05:57:29 +0100

    added the delete icon and also installed date-fns for a proper format of the date

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,5 +1,8 @@
 import { useWorkoutContext } from "../hooks/useWorkoutContext";
 
+//dates fns
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+
 const WorkoutDetails = ({ workout }) => {
   const { dispatch } = useWorkoutContext();
 
@@ -25,8 +28,12 @@ const WorkoutDetails = ({ workout }) => {
         <strong>Number of reps: </strong>
         {workout.reps}
       </p>
-      <p>{workout.createdAt}</p>
-      <span onClick={handleClick}>delete</span>
+      <p>
+        {formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}
+      </p>
+      <span onClick={handleClick} className="material-symbols-outlined">
+        delete
+      </span>
     </div>
   );
 };
