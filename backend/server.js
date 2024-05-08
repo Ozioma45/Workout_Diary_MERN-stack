9504da3bef830e60784e4bd431f8e4171bf14b51
Ozioma45 <oziomaegole@gmail.com>
2024-05-08 05:01:14 +0100

    user Routes, Controller & Model

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const workoutRoutes = require("./routes/workouts");
+const UserRoutes = require("./routes/user");
 const mongoose = require("mongoose");
 
 //express app
@@ -16,6 +17,7 @@ app.use((req, res, next) => {
 
 //route handler_react to request
 app.use("/api/workouts", workoutRoutes);
+app.use("/api/user", UserRoutes);
 
 //connect to DB
 mongoose
