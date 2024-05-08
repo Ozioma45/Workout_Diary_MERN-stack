9d536f60ddec95f67e7509b4cf0f9080a5bf4cc8
Ozioma45 <oziomaegole@gmail.com>
2024-05-08 06:32:43 +0100

    signing up user and password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 //login user
 const loginUser = async (req, res) => {
@@ -7,7 +8,15 @@ const loginUser = async (req, res) => {
 
 //signup user
 const signupUser = async (req, res) => {
-  res.json({ msg: "Signup user" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = { signupUser, loginUser };
