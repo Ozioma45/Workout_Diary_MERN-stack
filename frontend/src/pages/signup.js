68597acf1ab7f85cfc19243f03b5facf388576c6
Ozioma45 <oziomaegole@gmail.com>
2024-05-09 08:26:35 +0100

    react auth context

---
+++ frontend/src/pages/signup.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+<form className="signup" onSubmit={}>
+<h3>Sign up</h3>
+
+<label>Email: </label>
+<input
+type="email"
+onChange={(e)=>setEmail(e.target.value)}
+value={email}
+/>
+
+<label>Password: </label>
+<input
+type="password"
+onChange={(e)=>setPassword(e.target.value)}
+value={password}
+/>
+
+
+</form>  
+);
+};
+
+export default signup;
