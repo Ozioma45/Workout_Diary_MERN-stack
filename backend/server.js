5ae9c3f64828d31b14e3e826518d64ec48c1b9bd
Ozioma45 <oziomaegole@gmail.com>
2024-05-14 14:57:04 +0100

    removed unneccesary codes

--- backend/server.js
+++ backend/server.js
@@ -20,20 +20,6 @@ app.use((req, res, next) => {
 app.use("/api/workouts", workoutRoutes);
 app.use("/api/user", UserRoutes);
 
-//DEPLOYMENT
-const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../frontend/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is Running Successfully");
-  });
-}
-
 //connect to DB
 mongoose
   .connect(process.env.MONGO_URI)
